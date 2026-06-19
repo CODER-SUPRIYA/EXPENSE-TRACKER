1f9c39a8384968655ca1f615e22a48f047dc0efd
CODER-SUPRIYA <supriyas.ldas13012005@gmail.com>
2026-06-19 20:06:41 +0530

    Expense model and routes added,fixed package.json

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch((err) => console.log('MongoDB connection error:', err))
+
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Server is working!' })
 })
+
+const expenseRoutes = require('./routes/expenses')
+app.use('/api/expenses', expenseRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
