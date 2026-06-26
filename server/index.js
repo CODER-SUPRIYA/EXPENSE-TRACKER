c931d67e50e9f82cfe3dc2a925de70992970f82a
CODER-SUPRIYA <supriyas.ldas13012005@gmail.com>
2026-06-26 12:32:45 +0530

    fix CORS for production

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "https://expense-tracker-pi-cyan-60.vercel.app",
+  credentials: true
+}))
 app.use(express.json())
 
 app.get('/api/test', (req, res) => {
